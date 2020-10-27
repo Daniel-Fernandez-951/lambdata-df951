944391b6963a4e7d23e4386fada6a1dc9bd2356e
DanielFernández <n231bb@gmail.com>
2020-10-26 23:53:56 -0700

    Change to all OS

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: Mac, Windows",
+        "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
 )
