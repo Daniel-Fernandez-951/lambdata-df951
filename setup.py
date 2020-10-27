41c4e237e1f270e5fa0540e008acf950b15fa422
DanielFernández <n231bb@gmail.com>
2020-10-26 23:33:35 -0700

    Configure `setup.py` for PyPi module

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+REQUIRED = ['numpy', 'pandas', 'subprocess', 'platform', 'tabulate']
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="lamdata-df951", # Replace with your own username
+    version="0.0.1",
+    author="Daniel-Fernandez-951",
+    description="Example Collection of DS Modules",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Daniel-Fernandez-951/lambdata-df951",
+    packages=setuptools.find_packages(),
+    install_requires=REQUIRED,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Darwin & Windows",
+    ],
+    python_requires='>=3.6',
+)
